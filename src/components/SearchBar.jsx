f6fbb285b3d48810b64b5793577d7473fd026d75
Pandau <pandau.ting@gmail.com>
2024-12-17 17:20:54 +0100

    style searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import './SearchBar.css'
 
 import React from 'react'
+import { useEffect, useRef } from 'react'
 
 function SearchBar() {
   const navigate = useNavigate()
@@ -10,15 +11,40 @@ function SearchBar() {
     if (e.key == 'Enter') navigate('/search?s=' + e.target.value)
   }
 
+  // Ctrl + K: Focus Input Field
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.ctrlKey && event.key === 'k') {
+        event.preventDefault()
+        inputRef.current.focus()
+      }
+      if (event.key === 'Escape') {
+        event.preventDefault()
+        inputRef.current.blur()
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
   return (
     <div className='searchbar-container'>
+      <div className='glas'>🔍</div>
       <input
         type='text'
         name='movieInput'
         id='movieInput'
-        placeholder='🔍 SearchBar'
+        placeholder='Type the title of your favourite movie here...'
         onKeyDown={handleSubmit}
+        ref={inputRef}
       />
+      <div className='shortcut'>Ctrl+K</div>
     </div>
   )
 }
