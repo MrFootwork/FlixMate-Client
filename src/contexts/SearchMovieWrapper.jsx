cc7fb98b8007e636363279c247e358ce4ce7fa16
Pandau <pandau.ting@gmail.com>
2024-12-20 00:51:52 +0100

    forgot to push

--- src/contexts/SearchMovieWrapper.jsx
+++ src/contexts/SearchMovieWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import axios from 'axios'
 
 import config from '../../config'
